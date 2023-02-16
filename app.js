f33cbcc0b1d3dd1ad8b0c05750c82b53239397f1
Yusuf Bil <104075393+yusufbil54@users.noreply.github.com>
2023-02-16 15:38:47 +0300

    course.js commit

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute=require('./routes/pageRoute');
+const courseRoute=require('./routes/courseRoute');
+
+
 const app = express();
+//Connect DB
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost/smartedu-db',{
+  /*userNewUrlParser:true,
+  useUnifiedTopology:true,
+  useFindAndModify:false,
+  useCreateIndex:true*/
+}).then(()=>{
+  console.log('DB connected successfuly');
+});
 
 //Template Engine
 app.set("view engine","ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index',{
-    page_name:'index'
-  });
-});
-app.get('/about', (req, res) => {
-  res.status(200).render('about',{
-    page_name:'about'
-  });
-});
-
+//Routes
+app.use('/', pageRoute);
+app.use('/courses',courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
